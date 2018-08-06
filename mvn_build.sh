ee1521e9eec8d710bf485fa3628d2eabde6e37c0
leeway-deng <13146209849@163.com>
2018-08-06 18:30:36 +0800

    sort configs

--- mvn_build.sh
+++ mvn_build.sh
@@ -5,6 +5,7 @@ cur_dir=$(cd "$(dirname "${0}")"; pwd)
 
 module=$1
 cmd=$2
+biz=$3
 
 if [ -z "$module" ]; then
   echo "The building target module is invalid"
@@ -12,7 +13,14 @@ if [ -z "$module" ]; then
 fi
 
 if [ -z "$cmd" ]; then
-    cmd="package"
+  cmd="package"
+fi
+
+if [ -n "$biz" ] && [ -f "$module/src/main/resources/bootstrap-$biz.yml" ]; then
+  if [ ! -f "$module/src/main/resources/bootstrap.yml.bak" ]; then
+    cp -pf $module/src/main/resources/bootstrap.yml $module/src/main/resources/bootstrap.yml.bak
+  fi
+  cp -f $module/src/main/resources/bootstrap-$biz.yml $module/src/main/resources/bootstrap.yml
 fi
 
 mvn -B -e -f $module/pom.xml clean $cmd
