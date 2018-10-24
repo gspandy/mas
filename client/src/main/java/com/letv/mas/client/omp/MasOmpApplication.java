3c81688e6eb2d8f1f217ada619309807d8b5a83d
张帅 <wy1183130867@163.com>
2018-10-24 17:58:03 +0800

    结构调整

--- client/src/main/java/com/letv/mas/client/omp/MasOmpApplication.java
+++ client/src/main/java/com/letv/mas/client/omp/MasOmpApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @MapperScan("com.letv.mas.mapper")
 @ServletComponentScan
-public class MasOmpApplication {
+public class MasOmpApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(MasOmpApplication.class, args);
